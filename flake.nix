4a85e01fa52d997106757df4db04733e18317b4b
vlad <vlad@MacBook-Pro-vlad.local>
2024-12-12 21:14:15 +0300

    add system configuration

--- flake.nix
+++ flake.nix
@@ -10,12 +10,87 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
   let
+    username = "vlad";
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
         [ pkgs.vim
         ];
+
+      system = {
+        # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
+        activationScripts.postUserActivation.text = ''
+          # activateSettings -u will reload the settings from the database and apply them to the current session,
+          # so we do not need to logout and login again to make the changes take effect.
+          /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+        '';
+
+        defaults = {
+          dock = {
+            autohide = true;
+            autohide-delay = 0.1;
+            autohide-time-modifier = 0.5;
+            expose-group-apps = true;
+            mineffect = "scale";
+            mru-spaces = false;
+            persistent-apps = [];
+            persistent-others= [ "/Users/${username}/Downloads" ];
+            show-process-indicators = true;
+            show-recents = false;
+
+            # bottom-right corner action. 
+            wvous-br-corner = 4; # show desktop
+          };
+
+          finder = {
+            AppleShowAllExtensions = true;
+            AppleShowAllFiles = true;
+            CreateDesktop = false;
+            FXPreferredViewStyle = "clmv";
+            FXRemoveOldTrashItems = true;
+            NewWindowTarget = "Home";
+            ShowExternalHardDrivesOnDesktop = false;
+            ShowPathbar = true;
+            ShowRemovableMediaOnDesktop = false;
+            ShowStatusBar = true;
+          };
+
+          hitoolbox.AppleFnUsageType = "Show Emoji & Symbols";
+
+          loginwindow.GuestEnabled = false;
+
+          controlcenter.BatteryShowPercentage = true;
+          controlcenter.Bluetooth = true;
+
+          menuExtraClock.Show24Hour = true;
+
+          trackpad = {
+            ActuationStrength = 0;
+            FirstClickThreshold = 0;
+          };
+
+          NSGlobalDomain = {
+            AppleInterfaceStyle = "Dark";
+            "com.apple.sound.beep.feedback" = 0;
+          };
+
+          CustomUserPreferences = {
+            "com.apple.dock" = {
+              showMissionControlGestureEnabled = 1;
+              showAppExposeGestureEnabled = 1;
+            };
+            "com.apple.driver.AppleBluetoothMultitouch.trackpad" = {
+              TrackpadFourFingerVertSwipeGesture = 0;
+              TrackpadThreeFingerVertSwipeGesture = 2;
+            };
+            "com.apple.AppleMultitouchTrackpad" = {
+              TrackpadFourFingerVertSwipeGesture = 0;
+              TrackpadThreeFingerVertSwipeGesture = 2;
+            };
+          };
+        };
+      };
 
       homebrew = {
         enable = true;
@@ -79,7 +154,7 @@
           nix-homebrew = {
             enable = true;
             enableRosetta = true;
-            user = "vlad";
+            user = username;
 
             autoMigrate = true;
           };
